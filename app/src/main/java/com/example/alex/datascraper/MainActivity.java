4f60079d155d1a7f3a780d20a6af47f0084145f0
Alex <wootsy12@gmail.com>
2017-11-20 20:51:17 -0500

    Font size buffed up

--- app/src/main/java/com/example/alex/datascraper/MainActivity.java
+++ app/src/main/java/com/example/alex/datascraper/MainActivity.java
@@ -77,8 +77,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
 
 
 
